7119288602d803d3e4462e787e17478e461913e0
m.ramos257 <m.ramos257@gmail.com>
2025-10-20 02:47:34 +0000

    correccion en la fecha

--- src/app/loans/page.tsx
+++ src/app/loans/page.tsx
@@ -95,7 +95,10 @@ const LoanList = () => {
 
   const formatDate = (dateString: string) => {
     if (!dateString) return '';
-    const [year, month, day] = dateString.split('-');
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   };
 
